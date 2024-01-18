a718e3758a6b846222ba1dadbea4e309ba975c56
TuNN <tus.nngoc@gmail.com>
2024-01-18 22:37:23 +0700

    change files

--- QuizPractice/src/java/entity/Users.java
+++ QuizPractice/src/java/entity/Users.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package entity;
-
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 /**
  *
  * @author HP
  */
+
+@Builder
+@Getter
+@Setter
+@ToString
 public class Users {
+    private int userID; //change
+    private String fullname;
+    private String mobile;
+    private String username;
+    private String email;
+//    private enum status {
+//        ACTIVE,
+//        INACTIVE //change
+//    }
+    private boolean accountActived ; //change from status
+    private Role role; //add
+    
+    
     
 }
